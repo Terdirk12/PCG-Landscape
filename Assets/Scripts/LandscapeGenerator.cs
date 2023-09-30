9120e1512b801a5bf0d5cc9a83b5462a587d4dbc
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2023-09-30 13:17:59 +0200

    added normal map and perlin noise

--- Assets/Scripts/LandscapeGenerator.cs
+++ Assets/Scripts/LandscapeGenerator.cs
@@ -5,36 +5,64 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class LandscapeGenerator : MonoBehaviour
 {
-    public int xSize, ySize;
+    public int xSize, zSize;
     private Vector3[] vertices;
     private Mesh mesh;
+    public float scale = 5f;
     private void Awake()
     {
-       StartCoroutine(Generate());
+        StartCoroutine(Generate());
     }
 
     private IEnumerator Generate()
     {
-        WaitForSeconds wait = new WaitForSeconds(0.05f);
+        WaitForSeconds wait = new WaitForSeconds(0.0005f);
 
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "Procedural Grid";
 
-        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-        for (int i = 0, y = 0; y <= ySize; y++)
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+
+        // Modify the vertices to create a mountain using Perlin noise
+        // Adjust this scaling factor to control mountain height
+
+
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
             for (int x = 0; x <= xSize; x++, i++)
             {
-                vertices[i] = new Vector3(x, y);
-                yield return wait;
+                float xCoord = (float)x / xSize * scale;
+                float zCoord = (float)z / zSize * scale;
+
+                // Use Perlin noise to generate the mountain height
+                float mountainHeight = Mathf.PerlinNoise(xCoord, zCoord) * scale;
+
+                vertices[i] = new Vector3(x, mountainHeight, z);
+                uv[i] = new Vector2((float)x / xSize, (float)z / zSize);
+                tangents[i] = tangent;
             }
         }
         mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
 
-        int[] triangles = new int[3];
-        triangles[0] = 0;
-        triangles[1] = 1;
-        triangles[2] = xSize + 1;
+        int[] triangles = new int[xSize * zSize * 6];
+        for (int tri = 0, ver = 0, z = 0; z < zSize; z++, ver++)
+        {
+            for (int x = 0; x < xSize; x++, tri += 6, ver++)
+            {
+                triangles[tri] = ver;
+                triangles[tri + 3] = triangles[tri + 2] = ver + 1;
+                triangles[tri + 4] = triangles[tri + 1] = ver + xSize + 1;
+                triangles[tri + 5] = ver + xSize + 2;
+
+                mesh.triangles = triangles;
+                yield return wait;
+            }
+        }
     }
 
     private void OnDrawGizmos()
